549bf5511cb17839617fe4cb28372cc55fde2e13
asmithie13 <ajs392@pitt.edu>
2025-02-13 17:44:46 -0500

    Testbench code cleaned up

--- Pi App Code/Testbench.py
+++ Pi App Code/Testbench.py
@@ -2,12 +2,7 @@ import numpy as np
 from gameBoard import *
 from gamePiece import *
 
-def processMove(gb, visArr):
-    s = input("Enter move coordinates in #### form: ")
-    nums = np.array([["0", "1", "2", "3", "4", "5", "6", "7"]])
-    move = Move([int(s[0]), int(s[1])], [int(s[2]), int(s[3])])
-    result = gb.validateMove(move)
-
+def fillVisual(gb, visArr):
     for i in range(8):
         for j in range(8):
             if gb.tiles[i][j] is not None:
@@ -18,6 +13,13 @@ def processMove(gb, visArr):
             else:
                 visArr[i][j] = " "
 
+def processMove(gb, visArr):
+    s = input("Enter move coordinates in #### form: ")
+    nums = np.array([["0", "1", "2", "3", "4", "5", "6", "7"]])
+    move = Move([int(s[0]), int(s[1])], [int(s[2]), int(s[3])])
+    result = gb.validateMove(move)
+
+    fillVisual(gb, visArr)
 
     print(result)
     print(nums)
@@ -28,13 +30,7 @@ gb = GameBoard()
 
 visArr = [[" "] * 8 for _ in range(8)]
  
-for i in range(8):
-    for j in range(8):
-        if gb.tiles[i][j] is not None:
-            if gb.tiles[i][j].player == Player.BLACK:
-                visArr[i][j] = "B"
-            else:
-                visArr[i][j] = "R"
+fillVisual(gb, visArr)
 
 nums = np.array([["0", "1", "2", "3", "4", "5", "6", "7"]])
 visArr = np.array(visArr)
