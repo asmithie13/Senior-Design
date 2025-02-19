66cfe4e3c51db7a15fc2f8ed2f13d6204cbe5094
Alex Smith <tsmithie14@gmail.com>
2025-02-19 14:02:37 -0500

    Device address added and sample text send is complete

--- Arduino Code/send_serial_data.py
+++ Arduino Code/send_serial_data.py
@@ -3,7 +3,7 @@ import serial
 import time
 
 #Initialize Serial object to connect to HC-05 Bluetooth module:
-bluetoothObject=serial.Serial("/dev/rfcomm0", 9600, timeout=1) #Ensure that the HC-05 module is connected to "rfcomm0"
+bluetoothObject=serial.Serial("/dev/rfcomm0", 9600, timeout=10) #Ensure that the HC-05 module is connected to "rfcomm0"
 
 #Uncomment the line below, and comment-out line 6 if testing with Windows device instead of Pi:
 #bluetoothObject=serial.Serial("COM4", 9600, timeout=1) 
@@ -14,3 +14,6 @@ time.sleep(2)
 #Send the data:
 tempData="A1"
 bluetoothObject.write(tempData.encode())
+
+'''Device 98:D3:71:F7:0D:84 Name: HC-05
+[CHG] Device 98:D3:71:F7:0D:84 Alias: HC-05'''
