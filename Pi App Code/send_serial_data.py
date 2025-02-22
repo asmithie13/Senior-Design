45ea7e4ded7827e1b4199bd22ed8863a0f3e7289
20lillianjones <lej40@pitt.edu>
2025-02-21 21:14:45 -0500

    finish

--- Pi App Code/send_serial_data.py
+++ Pi App Code/send_serial_data.py
@@ -3,14 +3,14 @@ import serial
 import time
 
 #Initialize Serial object to connect to HC-05 Bluetooth module:
-bluetoothObject=serial.Serial("/dev/rfcomm0", 9600, timeout=1) #Ensure that the HC-05 module is connected to "rfcomm0"
+#bluetoothObject=serial.Serial("/dev/rfcomm0", 9600, timeout=1) #Ensure that the HC-05 module is connected to "rfcomm0"
 
 #Uncomment the line below, and comment-out line 6 if testing with Windows device instead of Pi:
-#bluetoothObject=serial.Serial("COM4", 9600, timeout=1) 
+bluetoothObject=serial.Serial("COM7", 9600, timeout=1) 
 
 #Ensure that the connection is established:
 time.sleep(2)
 
 #Send the data:
-tempData="A1"
+tempData="2031"
 bluetoothObject.write(tempData.encode())
