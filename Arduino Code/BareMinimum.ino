6e03c51229eb8cb51a35f646ae583f5359410ffe
aos15 <122504112+aos15@users.noreply.github.com>
2025-02-26 23:12:38 -0500

    Add files via upload
    Basic hard code for Max7219

---
+++ Arduino Code/BareMinimum.ino
@@ -0,0 +1,46 @@
+#include <MaxMatrix.h>
+
+#define maxDisplays 1  // Adjust for the number of MAX7219 chips
+#define dataPin 11     // DIN pin
+#define loadPin 10     // CS pin
+#define clockPin 13    // CLK pin
+
+MaxMatrix matrix(dataPin, loadPin, clockPin, maxDisplays);
+
+void setup() {
+  matrix.init();
+  matrix.setIntensity(8);  // Set brightness (current limit) 0 to 15
+}
+
+void loop() {
+  matrix.clear();  // Clear the display
+  matrix.setDot(0,1, 0xFF);
+  matrix.setDot(1,1, 0xFF);
+  matrix.setDot(2,1, 0xFF);
+  matrix.setDot(0,3, 0xFF);
+  matrix.setDot(1,3, 0xFF);
+  matrix.setDot(2,3, 0xFF);
+  matrix.setDot(0,5, 0xFF);
+  matrix.setDot(1,5, 0xFF);
+  matrix.setDot(2,5, 0xFF);
+  matrix.setDot(0,7, 0xFF);
+  matrix.setDot(1,7, 0xFF);
+  matrix.setDot(2,7, 0xFF);
+
+  matrix.setDot(7,0, 0xFF);
+  matrix.setDot(6,0, 0xFF);
+  matrix.setDot(5,0, 0xFF);
+  matrix.setDot(7,2, 0xFF);
+  matrix.setDot(6,2, 0xFF);
+  matrix.setDot(5,2, 0xFF);
+  matrix.setDot(7,4, 0xFF);
+  matrix.setDot(6,4, 0xFF);
+  matrix.setDot(5,4, 0xFF);
+  matrix.setDot(7,6, 0xFF);
+  matrix.setDot(6,6, 0xFF);
+  matrix.setDot(5,6, 0xFF);
+
+
+
+  delay(500);
+}
