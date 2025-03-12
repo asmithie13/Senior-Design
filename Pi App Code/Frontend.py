1f62f9e5c423188cf7db7d574c1025e87a3aa1f6
asmithie13 <ajs392@pitt.edu>
2025-03-12 17:14:33 -0400

    Main defined in frontend.py

--- Pi App Code/Frontend.py
+++ Pi App Code/Frontend.py
@@ -1,18 +1,19 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6 import uic  # Used to load .ui file dynamically
+from PyQt6 import uic
 import sys
-from Backend import Backend  # Import backend worker
+from Backend import Backend
 
 class Frontend(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('Pi App Code\Frontend_UI.ui', self)
 
-def main():
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Frontend()
+    backend = Backend()
+
+    #window.lastMove.connect(backend.latestMove)
+
     window.show()
     sys.exit(app.exec())
-
-if __name__ == "__main__":
-    main()
