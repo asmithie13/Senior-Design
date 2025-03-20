6663d37f5614bb0da87293f877d87525d20ab2e4
asmithie13 <ajs392@pitt.edu>
2025-03-20 11:24:46 -0400

    Enums changed to reflect red  and blue

--- Pi App Code/Objects/enums.py
+++ Pi App Code/Objects/enums.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 class Player(Enum):
-    BLACK = 1
-    RED = 2
+    RED = 1
+    BLUE = 2
 
 class MoveType(Enum):
     NON_DOUBLE_JUMP_MOVE = 0
@@ -14,8 +14,8 @@ class MoveError(str, Enum):
     START_TILE_EMPTY = "Invalid move: Start tile is empty."
     END_TILE_OCCUPIED = "Invalid move: End tile is occupied."
     WRONG_PLAYER_PIECE = "Invalid move: You can only move your own pieces."
-    BLACK_WRONG_DIRECTION = "Invalid move: BLACK pieces must move downward."
-    RED_WRONG_DIRECTION = "Invalid move: RED pieces must move upward."
+    RED_WRONG_DIRECTION = "Invalid move: RED pieces must move downward."
+    BLUE_WRONG_DIRECTION = "Invalid move: BLUE pieces must move upward."
     NOT_DIAGONAL = "Invalid move: Pieces must move diagonally."
     INVALID_DISTANCE = "Invalid move: Distance moved not 1 or 2 across."
     NO_PIECE_TO_CAPTURE = "Invalid move: No piece is being taken with this 2 distance move."
@@ -27,9 +27,9 @@ class MoveSuccess(str, Enum):
     CAPTURE_PIECE = "Piece is overtaken"
     DOUBLE_JUMP = "Piece is overtaken. Must double jump."
     GAME_OVER = "A player has no more pieces to play."
-    TIED_GAME = "Game is tied. Black pieces = Red pieces"
-    BLACK_WINS = "Black wins. Black pieces > Red pieces"
-    RED_WINS = "Black wins. Red pieces > Black pieces"
+    TIED_GAME = "Game is tied. Red pieces = Blue pieces"
+    RED_WINS = "Red wins. Red pieces > Blue pieces"
+    BLUE_WINS = "Blue wins. Blue pieces > Red pieces"
 
 class PhoneError(str, Enum):
     DECODE_ERROR = "Failed to decode data sent from phone"
